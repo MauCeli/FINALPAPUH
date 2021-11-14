c8d4a052971dba2125f354a84b4caae2bc56d0df
Celi <vmceli64@fi.unju.edu.ar>
2021-11-14 02:41:56 -0300

    first commit

---
+++ Assets/Scripts/DetectarColision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectarColision : MonoBehaviour
+{
+    int cantidadVida = 3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider colision)
+    {
+        if (colision.gameObject.tag == "enemigo1")
+        {
+            Debug.Log("Detecto collision");
+            cantidadVida--;
+            if (cantidadVida == 0)
+            {
+                Destroy(gameObject);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Generador.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generador : MonoBehaviour
+{
+    public GameObject prefab;
+    public float tiempoInicio = 0;
+    public float tiempoRep = 5;
+    void Start()
+    {
+        InvokeRepeating("Generar", tiempoInicio, tiempoRep);
+    }
+
+    // Update is called once per frame
+    void Generar()
+    {
+        Instantiate(prefab, transform.position, transform.rotation);
+    }
+}
