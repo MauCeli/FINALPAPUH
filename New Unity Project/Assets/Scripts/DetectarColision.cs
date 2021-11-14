e55daf5cda020177f88e0db4b6411a7e29d221c3
Celi <vmceli64@fi.unju.edu.ar>
2021-11-14 19:46:44 -0300

    nuevo commit

---
+++ New Unity Project/Assets/Scripts/DetectarColision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DetectarColision : MonoBehaviour
+{
+    int cantidadVida = 3;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider colision)
+    {
+        if (colision.gameObject.tag == "enemigo1")
+        {
+            Debug.Log("Detecto collision");
+            cantidadVida--;
+            if (cantidadVida == 0)
+            {
+                finJuego();
+                Destroy(gameObject);
+            }
+
+        }
+    }
+
+    public void finJuego()
+    {
+        SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Mover.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Mover : MonoBehaviour
+{
+    public float velx = 10;
+    public float jump = 6f;
+    public Rigidbody rb;
+    public int puntuacion;
+    public Text pts;
+
+    void Awake()
+    {
+        puntuacion = 0;
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        actualizarPts();
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {   
+      
+       
+            transform.Translate(Input.GetAxis("Horizontal") * velx * Time.deltaTime, 0, 0);
+ 
+        
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jump;
+            enabled = false;
+        }
+    }
+    private void OnCollisionEnter(Collision colision)
+    {
+        enabled = true;
+        actualizarPts();
+    }
+    void OnTriggerEnter(Collider obj)
+    {
+        puntuacion++;
+        obj.gameObject.SetActive(false);
+        actualizarPts();
+
+
+    }
+    void actualizarPts()
+    {
+        pts.text = "PUNTOS: " + puntuacion.ToString();
+    }
+
+}
